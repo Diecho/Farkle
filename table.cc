f0751c71f9c6f2ce23a977be9f0e8344fe16fbc2
kuang_1017333 <1017333@MY.SCCCD.EDU>
2025-12-01 22:51:23 -0800

    table

--- table.cc
+++ table.cc
@@ -10,20 +10,45 @@ Table::Table() {
 
 void Table::set_dice(vector<Die>* player_dice) {
     current_dice = player_dice;
+	for (int i = 0; i < 6; i++) {
+		held_dice.push_back(false);
+	}
 	//Create vector<bool> held_dice full of true 
 }
 
 void Table::roll_dices() {
-	//TODO
+	for (int i = 0; i < 6; i++) {
+		if(!(held_dice[i])) {
+			(*current_dice)[i].roll();
+		}
+	}
+
 }
 
 vector<ScoringOption> Table::possible_moves_calculation() {
 	//TODO
+	vector<int> num(6, 0);
+	for (int i = 0; i < 6; i++) {
+        if(!(held_dice[i])) {
+            int val = (*current_dice)[i].get_roll();
+			num[val-1] = num[val-1] + 1;
+        }
+    }
+	int count = 0;
+	for (int i = 0; i < 6; i++) {
+		count++;
+		if (num[i] > 3) {}
+
+	}
+
     return {}; 
 }
 
 void Table::perform_move(const ScoringOption& option) {
 	//TODO
+	
+
+
 }
 
 const vector<Die>& Table::get_dice() const {
